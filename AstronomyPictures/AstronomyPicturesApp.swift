845c6fdee12139211b5fc598ea8bb5bdd2da041b
PoojaBhatS <pooja.santhapadavau@stv.tv>
2024-08-19 12:48:40 +0100

    Initial Commit

---
+++ AstronomyPictures/AstronomyPicturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstronomyPicturesApp.swift
+//  AstronomyPictures
+//
+//  Created by Pooja on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct AstronomyPicturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
